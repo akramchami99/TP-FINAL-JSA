886d48c6227e49d74a1e972d0c1decf53e0e19cf
akramchami99 <62516333+akramchami99@users.noreply.github.com>
2023-10-26 13:50:14 +0200

    Question Fetch
    An API fetch to retrieve the question based on the options chosen by the player.

---
+++ src/Components/fetchQuestions.jsx
@@ -0,0 +1,24 @@
+function fetchQuestions (gameOptions) {
+	const { category, difficulty, type } = gameOptions;
+
+	let categoryParam = "";
+	let difficultyParam = "";
+	let typeParam = "";
+
+	if (category !== "")
+		categoryParam = `&category=${category}`;
+
+	if (difficulty !== "")
+		difficultyParam = `&difficulty=${difficulty}`;
+
+	if (type !== "")
+		typeParam = `&type=${type}`;
+
+	let apiUrl = `https://opentdb.com/api.php?amount=5${categoryParam}${difficultyParam}${typeParam}`;
+
+	return fetch(apiUrl)
+		.then(res => res.json())
+		.then(data => data.results);
+}
+
+export default fetchQuestions;
